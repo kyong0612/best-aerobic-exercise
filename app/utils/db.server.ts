974c9b4fd08b0d8d4b2513026c1a14d844eb7cb5
kimkiyong <fenderst0612@gmail.com>
2025-03-10 18:19:11 +0900

    Refactor database handling for production environment and update build script to copy database file

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,36 +1,35 @@
-import { PrismaClient } from '@prisma/client';
-import path from 'node:path';
-import fs from 'node:fs';
+import { PrismaClient } from "@prisma/client";
+import path from "node:path";
+import fs from "node:fs";
 
 declare global {
   // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient;
 }
 
-// Workaround to find the db file in production
-const filePath = path.join(process.cwd(), 'prisma/dev.db');
-const dbDir = path.dirname(filePath);
-
-// Ensure the directory exists
-if (!fs.existsSync(dbDir)) {
-  fs.mkdirSync(dbDir, { recursive: true });
-}
-
-// Fix: Use proper connection URL for read-write access
-const config = {
-  datasources: {
-    db: {
-      url: `file:${filePath}?connection_limit=1`,
-    },
-  },
-};
-
 let prisma: PrismaClient;
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
+  // Workaround to find the db file in production
+  const filePath = path.join(process.cwd(), "tmp/dev.db");
+  const dbDir = path.dirname(filePath);
+
+  // Ensure the directory exists
+  if (!fs.existsSync(dbDir)) {
+    fs.mkdirSync(dbDir, { recursive: true });
+  }
+
+  // Fix: Use proper connection URL for read-write access
+  const config = {
+    datasources: {
+      db: {
+        url: `file:${filePath}?connection_limit=1`,
+      },
+    },
+  };
   prisma = new PrismaClient(config);
 } else {
   if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient(config);
+    global.cachedPrisma = new PrismaClient();
   }
   prisma = global.cachedPrisma;
 }
